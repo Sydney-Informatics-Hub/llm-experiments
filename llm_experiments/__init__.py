92102a3437d80eb0ec7eb6f2b2e42188ff797c27
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2023-06-08 14:30:39 +1000

    feat(cotsc): Add CoTSC to package level imports

---
+++ llm_experiments/__init__.py
@@ -0,0 +1,6 @@
+from .cotsc import (CoTSC,
+                    SamplingScheme,
+                    create_cot_prompt_template,
+                    create_cot_prompt_example)
+
+__all__ = ['CoTSC', 'SamplingScheme', 'create_cot_prompt_template', 'create_cot_prompt_example']
