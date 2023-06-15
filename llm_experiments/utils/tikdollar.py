6afbd71e1b033919e31ff3a3741e8d273ca28bd4
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2023-06-15 11:53:48 +1000

    feat(tik): Add args: verbose, raise_err

--- llm_experiments/utils/tikdollar.py
+++ llm_experiments/utils/tikdollar.py
@@ -56,13 +56,16 @@ def count_input_tokens(model: str, prompt: str) -> int:
     return len(tokens)
 
 
-def tikdollar(cost_threshold: float, verbose: bool = False):
+def tikdollar(cost_threshold: float, raise_err: bool = True, verbose: bool = False):
     if isinstance(cost_threshold, int): cost_threshold = float(cost_threshold)
     if not isinstance(cost_threshold, float): raise TypeError("cost_threshold must be a float.")
     if not cost_threshold >= 0: raise ValueError("cost_threshold must be >= 0.")
-    # todo: check arg types and value ranges.
+    if not isinstance(raise_err, bool): raise TypeError("raise_err must be a boolean.")
+    if not isinstance(verbose, bool): raise TypeError("verbose must be a boolean.")
+
     cost_threshold: float = cost_threshold
-    verbose: bool = verbose
+    verbose = verbose
+    raise_err = raise_err
 
     def decorator(func: Callable):
         @functools.wraps(func)
@@ -80,20 +83,41 @@ def tikdollar(cost_threshold: float, verbose: bool = False):
             if not isinstance(llm, OpenAI) and not isinstance(llm, OpenAIChat):
                 raise TypeError("llm is not OpenAI or OpenAIChat langchain models.")
             if not isinstance(prompt, str):
-                raise TypeError("prompt must be a string. If you're using a prompt template, call .format() first.")
+                raise TypeError("prompt must be a str. If you're using a prompt template, call .format() first.")
 
             # todo: estimate cost from input tokens, optionally estimate output tokens.
             num_input_tokens: int
             num_output_tokens: int
             cost: float
 
-            num_input_tokens = count_input_tokens(model=llm.model_name, prompt=prompt)
+            num_input_tokens = count_input_tokens(llm.model_name, prompt)
             cost = get_openai_token_cost_for_model(
                 model_name=llm.model_name, num_tokens=num_input_tokens,
                 is_completion=False
             )
 
             min_cost_after_call = tikdollar_wrapper.tikdollar.cost + cost
+            if verbose:
+                print(f"Estimated cost on next request: ${cost:.8f}\tAccumulates to: ${min_cost_after_call:.8f}")
+            if min_cost_after_call > cost_threshold:
+                print(f"Estimated cost exceeds threshold: {min_cost_after_call} > {cost_threshold}.", file=sys.stderr)
+                if raise_err:
+                    raise CostThresholdReachedException(cost_threshold=cost_threshold, cost=min_cost_after_call)
+
+            cb: OpenAICallbackHandler
+            with get_openai_callback() as cb:
+                res = func(*args, **kwargs)
+                if num_input_tokens != cb.prompt_tokens:
+                    print(f"[warn] estimated input tokens did not match with callback count. "
+                          f"Expecting {num_input_tokens}. Got {cb.prompt_tokens}",
+                          file=sys.stderr)
+                num_input_tokens = cb.prompt_tokens
+                num_output_tokens = cb.completion_tokens
+                cost = cb.total_cost
+                if verbose: print(f"Actual cost on request: "
+                                  f"Input tokens={num_input_tokens}\t"
+                                  f"Output tokens={num_output_tokens}\t"
+                                  f"Cost={cost}")
             if min_cost_after_call > cost_threshold:
                 raise CostThresholdReachedException(cost_threshold=cost_threshold,
                                                     cost=min_cost_after_call)
