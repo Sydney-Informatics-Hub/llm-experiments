dad1b4fb17eb3861dd958e4694aed9b1ebe6ccf9
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2023-07-04 16:51:04 +1000

    feat(cot): Add CoTDataLeak

--- llm_experiments/cot.py
+++ llm_experiments/cot.py
@@ -4,20 +4,24 @@ A prompting technique used to encourage the model to generate a series of interm
 This script uses examples from the Chain of Thoughts paper.
 https://arxiv.org/abs/2201.11903
 """
+import sys
+
 from langchain.prompts import PromptTemplate, FewShotPromptTemplate
 from langchain.llms import OpenAI
 from typing import Optional
 
 __all__ = ['create_cot_prompt_example', 'create_cot_prompt_template',
-           'COT_EXAMPLE', 'COT_TEMPLATE']
+           'COT_EXAMPLE', 'COT_TEMPLATE',
+           'CoTDataLeak', 'CoTDataLeakException']
 
+# type aliases
 COT_EXAMPLE = dict[str, str]
 COT_TEMPLATE = FewShotPromptTemplate
 
-cot_prompt_template = PromptTemplate(
-    input_variables=['query', 'steps', 'answer'],
-    template="Query: {query}\nAnswer: {steps}. The answer is {answer}."
-)
+# constants
+_QUERY = 'query'
+_STEPS = 'steps'
+_ANS = 'answer'
 
 
 def create_cot_prompt_example(
@@ -26,10 +30,16 @@ def create_cot_prompt_example(
         answer: str,
 ) -> COT_EXAMPLE:
     return {
-        'query': query,
-        'steps': steps,
-        'answer': answer,
+        _QUERY: query,
+        _STEPS: steps,
+        _ANS: answer,
     }
+
+
+_cot_example_template = PromptTemplate(
+    input_variables=[_QUERY, _STEPS, _ANS],
+    template=f"Query: {{{_QUERY}}}\nAnswer: {{{_STEPS}}}. The answer is {{{_ANS}}}."
+)
 
 
 def create_cot_prompt_template(
@@ -48,17 +58,81 @@ def create_cot_prompt_template(
     if len(cot_examples) <= 0: raise ValueError("There must be at least 1 cot_example.")
 
     for ex in cot_examples:
-        if not {"query", "steps", "answer"} == set(ex.keys()):
-            raise ValueError(f"Missing keys (query, steps, answer) in example: {ex}")
+        if not {_QUERY, _STEPS, _ANS} == set(ex.keys()):
+            raise ValueError(f"Missing keys ({_QUERY}|{_STEPS}|{_ANS}) in example: {ex}")
 
     return FewShotPromptTemplate(
         prefix=instructions + "\n\n",
-        example_prompt=cot_prompt_template,
+        example_prompt=_cot_example_template,
         examples=cot_examples,
-        suffix="Query: {query}",
-        input_variables=['query'],
+        suffix=f"Query: {{{_QUERY}}}",
+        input_variables=[_QUERY],
         example_separator='\n',
     )
+
+
+class CoTDataLeakException(Exception):
+    def __init__(self, query: str):
+        self.query = query
+
+
+class CoTDataLeak(object):
+    """ Checks for Data Leakage given CoT examples used. """
+
+    CHECK_TYPES = ('exact',)
+
+    def __init__(self, template: COT_TEMPLATE, check_type: str = 'exact', raise_err: bool = True):
+        if not isinstance(template, COT_TEMPLATE):
+            raise TypeError("template must be a FewShotPromptTemplate/COT_TEMPLATE.")
+        if not check_type in self.CHECK_TYPES:
+            raise ValueError(f"check_type must be one of {', '.join(self.CHECK_TYPES)}")
+        if not isinstance(raise_err, bool):
+            raise TypeError("raise_err must be a boolean.")
+
+        queries = [ex.get("query", None) for ex in template.examples]
+        assert None not in queries, "Missing 'query' parameter in your CoT examples."
+        assert len(queries) == len(template.examples), "Mismatched number of CoT examples of number of queries."
+
+        self.queries = queries
+        self.template = template
+        self.check_type = check_type
+        self.raise_err = raise_err
+
+        self._setup: dict[str, Optional] = dict()
+
+    def check(self, query: str) -> bool:
+        """ Checks whether your query is leaked in your CoT examples."""
+        if self.check_type == 'exact':
+            matched = self.exact_matched(query)
+        else:
+            raise NotImplementedError(f"Missing implementation for {self.check_type}.")
+
+        if matched:
+            if self.raise_err: raise CoTDataLeakException(query)
+            else: print(f'[warn] query is leaked as part of CoT examples. {query}', file=sys.stderr)
+        return not matched
+
+    def exact_matched(self, query: str) -> bool:
+        if self._setup.get('exact', None) is None:
+            self._setup['exact'] = set(self.queries)  # performance
+        return query in self._setup['exact']
+
+
+### Unit Tests ###
+from unittest import TestCase
+
+
+class TestCoT(TestCase):
+    def test_create_cot_example(self):
+        ex = create_cot_prompt_example('query placeholder', 'steps placeholder', 'answer placeholder')
+        target = {'query': 'query placeholder', 'steps': 'steps placeholder', 'answer': 'answer placeholder'}
+        assert ex == target, "COT example created is invalid."
+
+    def test_dataleak(self):
+        ex = create_cot_prompt_example('query placeholder', 'steps placeholder', 'answer placeholder')
+        template = create_cot_prompt_template("", cot_examples=[ex])
+        dataleak = CoTDataLeak(template=template, raise_err=False)
+        assert not dataleak.check('query placeholder')
 
 
 ### Example used in the Chain of Thoughts Paper ###
@@ -120,6 +194,7 @@ if __name__ == '__main__':
     # uses /v1/completions endpoint
     # https://platform.openai.com/docs/models/model-endpoint-compatibility
 
+    print("This runs the examples in the CoT paper...")
     model = OpenAI(model_name='text-davinci-003',
                    # model_name='text-davinci-002',
                    # model_name='text-curie-001',
