1f4ffa48ab872fab45f8468dcfc52138b346fbd9
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2023-07-19 10:56:07 +1000

    feat(cot): Add CoT export to package root

--- llm_experiments/__init__.py
+++ llm_experiments/__init__.py
@@ -1,5 +1,5 @@
 from llm_experiments.cot.cotsc import (CoTSC,
                                        SamplingScheme)
-from llm_experiments.cot.cot import (create_cot_prompt_template, create_cot_prompt_example)
+from llm_experiments.cot.cot import (CoT, create_cot_prompt_example)
 
-__all__ = ['CoTSC', 'SamplingScheme', 'create_cot_prompt_template', 'create_cot_prompt_example']
+__all__ = ['CoTSC', 'SamplingScheme', 'CoT', 'create_cot_prompt_example']
