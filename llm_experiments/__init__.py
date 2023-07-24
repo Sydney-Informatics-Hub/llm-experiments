39ca5d0c121be093734930e14eb22cb6032612be
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2023-07-24 12:23:28 +1000

    refactor: Add CoT imports from .cot and root

--- llm_experiments/__init__.py
+++ llm_experiments/__init__.py
@@ -1,5 +1,3 @@
-from llm_experiments.cot.cotsc import (CoTSC,
-                                       SamplingScheme)
-from llm_experiments.cot.cot import (CoT, create_cot_prompt_example)
+from .cot import (CoTSC, SamplingScheme, CoT, create_cot_prompt_example)
 
 __all__ = ['CoTSC', 'SamplingScheme', 'CoT', 'create_cot_prompt_example']
---
+++ llm_experiments/cot/__init__.py
@@ -0,0 +1,5 @@
+from .cotsc import (CoTSC,
+                    SamplingScheme)
+from .cot import (CoT, create_cot_prompt_example)
+
+__all__ = ['CoTSC', 'SamplingScheme', 'CoT', 'create_cot_prompt_example']
